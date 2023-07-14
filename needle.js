7ecdad9be88e1f9f2af11892e8b8a0815004d4c9
Gabin <paquesgabin@gmail.com>
2023-07-14 17:15:28 +0100

    Adding the ligns for the distance between the center of the needles and lattes

--- needle.js
+++ needle.js
@@ -15,4 +15,39 @@ class Needle{
         ellipse(this.x, this.y, 5, 5);
         pop();
     }
+
+    // Check if the needle is crossing a latte
+    nearestLatte(){
+        let r = Infinity;
+        let n = 0;
+        for(let i = 0; i < lattes.length; i++){
+            let dist = abs(this.x - lattes[i]);
+            if(dist < r){
+                r = dist;
+                n = i;
+            }
+        }
+        return n;
+    }
+
+    calculDistanceBetweenClosestLatte(){
+        let r = abs(this.x - lattes[this.nearestLatte()]);
+        return r;
+    }
+
+    drawDistanceBetweenLatte(){
+        push();
+        stroke(255, 0, 0);
+        if(this.x > lattes[this.nearestLatte()]){
+            line(this.x, this.y, this.x - this.calculDistanceBetweenClosestLatte(), this.y);
+            ellipse(this.x - this.calculDistanceBetweenClosestLatte(), this.y, 5);
+        }
+        else{
+            line(this.x, this.y, this.x + this.calculDistanceBetweenClosestLatte(), this.y);
+            ellipse(this.x + this.calculDistanceBetweenClosestLatte(), this.y, 5);
+        }
+
+
+        pop();
+    }
 }
--- sketch.js
+++ sketch.js
@@ -4,10 +4,8 @@ const CANVAS_HEIGHT = 600;
 var distBetweenTwoLattes = 100; // Distance between two lattes
 var lattes = []; // Array that contain the X position of each latte
 
-var numberOfNeedles = 1; // Number of needles
 var lenNeedles = 100; // Length of the needles
 var needles = []; // Array that contain all the needles
-
 
 /* ------------------------------------------------------------------------ */
 
@@ -33,7 +31,21 @@ function draw() {
   // Let's draw the needles
   for(let i = 0; i < needles.length; i++){
     needles[i].draw();
+    needles[i].drawDistanceBetweenLatte();
   }
+
+  // Messages
+  push();
+  textSize(20);
+  text("Number of needles: " + needles.length, CANVAS_WIDTH + 10, 20);
+  text("Size of the needles: " + lenNeedles, CANVAS_WIDTH + 10, 40);
+  text("Number of lattes: " + lattes.length, CANVAS_WIDTH + 10, 80);
+  text("Size of the lattes: " + distBetweenTwoLattes, CANVAS_WIDTH + 10, 100);
+  if(needles.length > 0){
+    text("Closest Lattes (x): " + needles[needles.length - 1].nearestLatte(), CANVAS_WIDTH + 10, 140);
+    text("Distance (x):" + needles[needles.length - 1].calculDistanceBetweenClosestLatte(), CANVAS_WIDTH + 10, 160);
+  }
+  pop();
 }
 
 
